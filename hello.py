be1366518abec2e07725f69c23bb251ad9ac6eae
bhenry4 <benji.paq1@gmail.com>
2023-08-29 15:08:06 -0400

    libs and code added

--- hello.py
+++ hello.py
@@ -1,5 +1,22 @@
-from time import sleep
+import pwmio, board, digitalio
+from adafruit_motor import servo
+import time
+
+pwm = pwmio.PWMOut(board.D12, duty_cycle=2**15, frequency=50)
+servo0 = servo.Servo(pwm)
+servoAngle = 90
+
+buttonLeft = digitalio.DigitalInOut(board.D10)
+buttonRight = digitalio.DigitalInOut(board.D9)
+buttonLeft.pull = digitalio.Pull.DOWN
+buttonRight.pull = digitalio.Pull.DOWN
 
 while True:
-    print("Hello world!!")
-    sleep(1)
+#    servo0.angle = servoAngle
+
+#    if buttonLeft.value:
+#        servoAngle -= 1
+#    if buttonRight.value:
+#        servoAngle += 1
+
+    print(buttonRight.value)
