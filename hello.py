953c51e0e3d016c1b2bbeff8ba4236686d5b5e71
bhenry4 <benji.paq1@gmail.com>
2023-08-29 15:25:20 -0400

    converted buttons to capacative touch

--- hello.py
+++ hello.py
@@ -1,4 +1,4 @@
-import pwmio, board, digitalio
+import pwmio, board, touchio
 from adafruit_motor import servo
 import time
 
@@ -6,10 +6,8 @@ pwm = pwmio.PWMOut(board.D12, duty_cycle=2**15, frequency=50)
 servo0 = servo.Servo(pwm)
 servoAngle = 90
 
-buttonLeft = digitalio.DigitalInOut(board.D10)
-buttonRight = digitalio.DigitalInOut(board.D9)
-buttonLeft.pull = digitalio.Pull.DOWN
-buttonRight.pull = digitalio.Pull.DOWN
+buttonLeft = touchio.TouchIn(board.A0)
+buttonRight = touchio.TouchIn(board.A1)
 
 while True:
     servo0.angle = servoAngle
