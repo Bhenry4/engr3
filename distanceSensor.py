ddbbc2b67708943de7cead5b4cc7b3dbc43103c0
bhenry4 <benji.paq1@gmail.com>
2023-09-12 15:21:50 -0400

    working neopixel

--- distanceSensor.py
+++ distanceSensor.py
@@ -4,12 +4,14 @@ import time
 
 trig = board.D8
 echo = board.D9
-sensor = adafruit_hcsr04.HCSR04(trig, echo)
-led = neopixel.NeoPixel(board.NEOPIXEL)
+#sensor = adafruit_hcsr04.HCSR04(trig, echo)
+led = neopixel.NeoPixel(board.NEOPIXEL, 1, brightness=0.01)
 
 while True:
-    try:
-        print(sensor.distance)
-    except:
-        print("Timed Out")
+    # try:
+    #     print(sensor.distance)
+    # except:
+    #     print("Timed Out")
+    # time.sleep(0.1)
+    led[0] = (255,0,255)
     time.sleep(0.1)
