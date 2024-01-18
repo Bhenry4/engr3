8a96fee5c1821459eed0e64f9e05aad9de09c688
bhenry4 <benji.paq1@gmail.com>
2024-01-18 16:02:39 -0500

    stepper motor working

--- stepperMotor.py
+++ stepperMotor.py
@@ -4,3 +4,25 @@ import keypad
 import time
 import digitalio
 from adafruit_motor import stepper
+
+DELAY = 0.01
+STEPS = 100
+
+coils = (
+    digitalio.DigitalInOut(board.D9),
+    digitalio.DigitalInOut(board.D10),
+    digitalio.DigitalInOut(board.D11),
+    digitalio.DigitalInOut(board.D12),
+)
+
+for coil in coils:
+    coil.direction = digitalio.Direction.OUTPUT
+motor = stepper.StepperMotor(*coils, microsteps=None)
+
+while True:
+    for i in range(STEPS):
+        motor.onestep(style=stepper.DOUBLE)
+        time.sleep(DELAY)
+    for i in range(STEPS):
+        motor.onestep(style=stepper.DOUBLE, direction=stepper.BACKWARD)
+        time.sleep(DELAY)
